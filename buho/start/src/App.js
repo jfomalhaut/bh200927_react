cdeb4960c677e9c68b85d2f5b12b2abc71451703
이준 <dlwnssla@jfomalhaut.com>
2020-10-08 19:10:42 +0900

    route

--- buho/start/src/App.js
+++ buho/start/src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 import { Home, List, Data, Items1, Items2, Items3 } from './routers';
 import Navigation from './components/Navigation';
+import Item from './routers/Items/Item';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/home" component={Home} />
         <Route path="/list" component={List} />
         <Route path="/data/:name" component={Data} />
+        <Route path="/item" component={Item} />
         <Route path="/items1" component={Items1} />
         <Route path="/items2" component={Items2} />
         <Route path="/items3" component={Items3} />
