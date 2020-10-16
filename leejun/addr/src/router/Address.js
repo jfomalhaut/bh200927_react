5eaf227bd7b42ad3d5b685122f26ef64b0835b7a
이준 <dlwnssla@jfomalhaut.com>
2020-10-16 11:38:10 +0900

    dd

--- leejun/addr/src/router/Address.js
+++ leejun/addr/src/router/Address.js
@@ -26,7 +26,9 @@ const Address = () => {
 		};
 
 		Axios.get(`${REQUEST_URL}?confmKey=${CONFIRM_KEY}&currentPage=${currentPage}&countPerPage=${VIEW}&resultType=json&keyword=${keyword}`).then(res => {
+			const { data: { results: { juso, common } } } = res;
 			console.log(res);
+			setList(juso);
 		}).catch(error => {
 			console.log(error);
 		}).finally(() => {
@@ -46,7 +48,12 @@ const Address = () => {
 				<button>검색</button>
 			</div>
 			<ul>
-				
+				{list.map(item => (
+					<li>
+						<div>{item.zipNo}</div>
+						<div>{item.roadAddrPart1}</div>
+					</li>
+				))}
 			</ul>
 		</Form>
 	);
@@ -55,5 +62,42 @@ const Address = () => {
 export default Address;
 
 const Form = styled.form`
+	width: 600px;
+	margin: 100px auto;
+	& > div {
+		display: flex;
+		button {
+			width: 100px;
+			flex-shrink: 0;
+			font-size: 20px;
+			outline: none;
+		}
+		input {
+			outline: none;
+			font-size: 20px;
+			flex-grow: 1;
+			padding: 10px;
+		}
+	}
+	ul {
+		list-style: none;
+		padding: 0;
+		li {
+			display: flex;
+			list-style: none;
+			padding: 15px 10px;
+			border-bottom: 1px solid #ddd;
 
+			div {
+				font-size: 20px;
+				&:first-child {
+					width: 100px;
+					flex-shrink: 0;
+				}
+				&:last-child {
+					flex-grow: 1;
+				}
+			}
+		}
+	}
 `;
