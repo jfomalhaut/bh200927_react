8dd8ecbdf6c12782f6014daa04ce5dd8a6cbbe1f
unknown <mose23@naver.com>
2020-10-25 11:17:10 +0900

    2

--- buho/samyang/src/App.js
+++ buho/samyang/src/App.js
@@ -1,21 +1,10 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
-
-import { Home, Address } from "./routers";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/home" component={Home} />
-        <Route path="/" component={Address} />
-      </Switch>
-    </Router>
+    <div>
+      <h2>hi</h2>
+    </div>
   );
 };
 
