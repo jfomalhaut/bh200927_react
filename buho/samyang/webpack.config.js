e4c01543ae5c25959933db632700b3e124215588
unknown <mose23@naver.com>
2020-10-25 11:10:50 +0900

    1

---
+++ buho/samyang/webpack.config.js
@@ -0,0 +1,38 @@
+const dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: ["./src/index.js"],
+  plugins: [new dotenv()],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(css)$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", "*"],
+  },
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: "./dist",
+    port: 3000,
+    historyApiFallback: true,
+  },
+};
